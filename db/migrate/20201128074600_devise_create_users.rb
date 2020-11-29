9ef6022693d03c6a0cff339029a6ef660f3a2e63
yoshi0531 <yoshi0531.com@gmail.com>
2020-11-29 22:25:02 +0900

    ログイン・サインアップ機能実装

--- db/migrate/20201128074600_devise_create_users.rb
+++ db/migrate/20201128074600_devise_create_users.rb
@@ -6,7 +6,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       ## Database authenticatable
       t.string :first_name,         null: false
       t.string :last_name,          null: false
-      t.string :email,              null: false, nuique: true, default: ""
+      t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
       t.string :nickname,           null: false
       t.string :first_name_reading, null: false
