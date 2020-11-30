d5726538de019a69b91c18421a2ce5444b2b365c
yoshi0531 <yoshi0531.com@gmail.com>
2020-11-30 20:01:52 +0900

    valideteの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,10 +9,10 @@ class User < ApplicationRecord
   has_many :comments
 
   with_options presence: true do
-    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters."}
-    validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters."}
+    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: "is invalid. Input full-width characters."}
+    validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: "is invalid. Input full-width characters."}
     validates :first_name_reading, format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters."}
     validates :last_name_reading, format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters."}
-    validates :encrypted_password, format: { with: /\A[a-z0-9]+\z/i, message: "is invalid. Input half-width characters."}
+    validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: "is invalid. Input half-width characters."}
   end
 end
