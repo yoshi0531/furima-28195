656e36cc6494db50793b1150b8f4babecbd0b94c
yoshi0531 <yoshi0531.com@gmail.com>
2020-12-02 17:35:09 +0900

    テストコード追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -23,6 +23,11 @@ RSpec.describe User, type: :model do
         @user.valid?
         expect(@user.errors.full_messages).to include("Email can't be blank")
       end
+      it "emailに@含めないと登録できない" do
+        @user.email = "aaaaaaaaaaaaa"
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Email is invalid")
+      end
       it "重複したemailが存在する場合登録できない" do
         @user.save
         another_user = FactoryBot.build(:user)
@@ -53,8 +58,19 @@ RSpec.describe User, type: :model do
         @user.valid?
         expect(@user.errors.full_messages).to include("Password is too short (minimum is 6 characters)")
       end
+      it "passwordに全角が入ると登録できない" do
+        @user.password = "ｂ1a1a"
+        @user.password_confirmation = "ｂ1a1a"
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password is too short (minimum is 6 characters)")
+      end
       it "passwordが存在してもpassword_confirmationが空では登録できない" do
         @user.password_confirmation = ""
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+      end
+      it "passwordとpassword_confirmationの値が同じでないと登録できない" do
+        @user.password_confirmation = "1a1a1a1a"
         @user.valid?
         expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
       end
