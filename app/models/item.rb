a96fe73208938700e16bec6e8f0199a64d781eab
yoshi0531 <yoshi0531.com@gmail.com>
2020-12-04 14:08:39 +0900

    商品出品ページへの遷移・Active Storageの導入

---
+++ app/models/item.rb
@@ -0,0 +1,17 @@
+class Item < ApplicationRecord
+  belongs_to :user
+  has_one    :orders
+  has_many   :comments
+  has_one_attached :image
+
+  with_options presence: true do
+    validates :name
+    validates :text
+    validates :category_id
+    validates :status_id
+    validates :cost_id
+    validates :prefecture_id
+    validates :days_id
+    validates :price
+  end
+end
