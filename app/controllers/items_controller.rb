fb455e57706b3e92a02c68444cb72d6a67e63569
yoshi0531 <yoshi0531.com@gmail.com>
2020-12-06 01:51:55 +0900

    controller修正、validate追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,8 +7,13 @@ class ItemsController < ApplicationController
   end
 
   def create
-    @item = Item.create item_params
-    redirect_to root_path
+    @item = Item.new(item_params)
+    if @item.valid?
+      @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   private
