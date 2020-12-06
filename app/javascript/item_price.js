945945c9d6a82956e09f8dce1f4cb5c841e71faf
yoshi0531 <yoshi0531.com@gmail.com>
2020-12-06 19:22:36 +0900

    javasprict修正

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -5,9 +5,10 @@ window.addEventListener('load', function(){
   priceInput.addEventListener('input', () => {
     const inputValue = priceInput.value;
     const addTaxDom = document.getElementById("add-tax-price");
-    let tax = 0.1;
-    const salesCommission = inputValue * tax;
-    addTaxDom.innerHTML = Math.floor(salesCommission);
+    let rate = 0.1;
+    const fee = inputValue * rate;
+    addTaxDom.innerHTML = Math.floor(fee);
+    const salesCommission = Math.floor(fee);
     const salesProfit = document.getElementById("profit");
     salesProfit.innerHTML = Math.floor(inputValue - salesCommission);
   })
