46b385a24a62539e23114ea4e20a326b4520644c
yoshi0531 <yoshi0531.com@gmail.com>
2020-12-06 16:49:28 +0900

    itemモデルのテストコード実装

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,13 +1,17 @@
 FactoryBot.define do
   factory :item do
-    nickname              {Faker::Name.initials(number: 6)}
-    email                 {Faker::Internet.free_email}
-    password              {'1a'+Faker::Internet.password(min_length: 6)}
-    password_confirmation {password}
-    birthday              {Faker::Date.between(from: '1930-01-01', to: '2015-12-31')}
-    first_name            {"次郎"}
-    last_name             {"フリマ"}
-    first_name_reading    {"ジロウ"}
-    last_name_reading     {"フリマ"}
+    name          {Faker::Commerce.product_name}
+    text          {Faker::Lorem.sentence}
+    category_id   {Faker::Number.between(from: 2, to: 11)}
+    status_id     {Faker::Number.between(from: 2, to: 7)}
+    cost_id       {Faker::Number.between(from: 2, to: 3)}
+    prefecture_id {Faker::Number.between(from: 2, to: 48)}
+    day_id        {Faker::Number.between(from: 2, to: 4)}
+    price         {Faker::Number.between(from: 300, to: 9999999)}
+    association :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,63 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @item = FactoryBot.build(:item)
+  end
+  
+  describe '商品出品登録' do
+    context '商品出品登録がうまくいく時' do
+      it "全ての値が正しく入力されていれば保存できること" do
+        expect(@item).to be_valid
+      end
+    end
+
+    context '商品出品登録がうまくいかない時' do
+      it "nameが空だと登録できないこと" do
+        @item.name = ""
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Name can't be blank")
+      end
+      it "textが空では登録できない" do
+        @item.text = ""
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Text can't be blank")
+      end
+      it "category_idが1だと登録できない" do
+        @item.category_id = "1"
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category must be other than 1")
+      end
+      it "status_idが1だと登録できない" do
+        @item.status_id = "1"
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Status must be other than 1")
+      end
+      it "cost_idが1だと登録できない" do
+        @item.cost_id = "1"
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Cost must be other than 1")
+      end
+      it "prefecture_idが1だと登録できない" do
+        @item.prefecture_id = "1"
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Prefecture must be other than 1")
+      end
+      it "day_idが1だと登録できない" do
+        @item.day_id = "1"
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Day must be other than 1")
+      end
+      it "priceが300未満だと登録できない" do
+        @item.price = "299"
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be greater than 300")
+      end
+      it "priceが10,000,000以上だと登録できない" do
+        @item.price = "10000000"
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be less than 9999999")
+      end
+    end
+  end
 end
