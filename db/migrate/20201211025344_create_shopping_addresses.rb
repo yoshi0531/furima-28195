e26bb158b198f49c4f9ac9109535568d2ee7f72b
yoshi0531 <yoshi0531.com@gmail.com>
2020-12-12 09:10:45 +0900

    データベース作成

--- db/migrate/20201211025344_create_shopping_addresses.rb
+++ db/migrate/20201211025344_create_shopping_addresses.rb
@@ -1,7 +1,13 @@
 class CreateShoppingAddresses < ActiveRecord::Migration[6.0]
   def change
     create_table :shopping_addresses do |t|
-
+      t.string     :postal_code,   null: false, default: ""
+      t.integer    :prefecture_id, null: false
+      t.string     :city,          null: false, default: ""
+      t.string     :house_number,  null: false, default: ""
+      t.string     :building_name, default: ""
+      t.string     :phone_number,  null: false, default: ""
+      t.references :order,         null: false, foreign_key: true
       t.timestamps
     end
   end
