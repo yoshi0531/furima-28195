5958a2948ff44f161ffb3866da9ee6ea08ba6dc8
yoshi0531 <yoshi0531.com@gmail.com>
2020-12-16 00:12:10 +0900

    ページ条件定義

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,12 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!
+
   def index
     @item = Item.find(params[:item_id])
     @buyer = Buyer.new
+    if current_user.id == @item.user.id
+      redirect_to root_path
+    end
   end
 
   def create
