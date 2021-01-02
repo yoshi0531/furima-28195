610f5a6390f5a31a8da5500feb5e6bc8f97c6d9b
yoshi0531 <yoshi0531.com@gmail.com>
2021-01-03 00:04:22 +0900

    ログイン機能実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,24 @@ class User < ApplicationRecord
   has_many :orders
   has_many :sns_credentials
 
+  def self.from_omniauth(auth)
+    sns = SnsCredential.where(provider: auth.provider, uid: auth.uid).first_or_create
+    # sns認証したことがあればアソシエーションで取得
+    # 無ければemailでユーザー検索して取得orビルド(保存はしない)
+    user = User.where(email: auth.info.email).first_or_initialize(
+      nickname: auth.info.name,
+      email: auth.info.email,
+      first_name: auth.info.first_name,
+      last_name: auth.info.last_name
+    )
+    # userが登録済みであるか判断
+    if user.persisted?
+      sns.user = user
+      sns.save
+    end
+    user
+  end
+
   with_options presence: true do
     validates :nickname
     validates :birthday
